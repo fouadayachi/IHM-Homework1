062a433513c48b758ff502e5cc6801935a060fc1
fouadayachi <ayachifouad20@gmail.com>
2024-11-03 17:19:19 +0100

    first commit

--- src/components/Input.js
+++ src/components/Input.js
@@ -8,7 +8,7 @@ export default function FormInput ({label,name,value,handleOnDataChange,type,err
                 <label className={`${error && "error"}`}>{error}</label>
             </div>
             {type === "text" ? <input className={`${error && "input-error"}`} type={type} name={name} id={name} value={value} onChange={handleOnDataChange}/> 
-            : <input className={`${error && "input-error"}`} type={type} name={name} id={name} value={value} onChange={handleOnDataChange} min="1900-01-01" max="2026-12-31"/>}
+            : <input className={`${error && "input-error"}`} type={type} name={name} id={name} value={value} onChange={handleOnDataChange} min="1900-01-01" max="2026-12-31" />}
         </div>
     )
 }
